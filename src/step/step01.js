b5c3ccd36f109cc74ec324b50ddc9a7ea00f57cd
yoon664 <yoonj0264@gmail.com>
2025-04-06 09:26:46 +0900

    .

--- src/step/step01.js
+++ src/step/step01.js
@@ -1,14 +1,27 @@
+import clsx from 'clsx';
 import React, { useState } from 'react'
 
 const Step01 = () => {
     const [isActive, setIsActive] = useState(false);
-  return (
-    <>
-        <button className={px-4 py-2 border ${isActive ? "bg-blue-500" : "bg-gray-200"}} onClick={()=>{setIsActive(!isActive)}}>
-            {isActive ? "활성화" : "비활성화"}
-        </button>
-    </>
-  )
+    return (
+        <>
+            {/* <button
+                className={`px-4 py-2 border ${isActive ? "bg-blue-500" : "bg-gray-200"} `}
+                onClick={()=>{setIsActive(!isActive)}}
+            >
+                {isActive ? "활성화" : "비활성화"}
+            </button> */}
+            <button
+                className={clsx('px-4 py-2 border',
+                    isActive && 'bg-blue-500',
+                    !isActive && 'bg-gray-500',
+                )}
+                onClick={()=>{setIsActive(!isActive)}}
+            >
+                {isActive ? "활성화" : "비활성화"}
+            </button> 
+        </>
+    )
 }
 
 export default Step01
