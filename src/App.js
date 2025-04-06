28fd4c0de2e4ec7c2a50891c7b9c54ce93bf8f1e
yoon664 <yoonj0264@gmail.com>
2025-04-06 09:38:17 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Step01 from './step/step01';
+import Step02 from './step/step02';
 
 function App() {
   return (
     <>
-      <Step01 />
+      {/* <Step01 /> */}
+      <Step02 />
     </>
   );
 }
